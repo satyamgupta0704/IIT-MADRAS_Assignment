60547837880e29372a18c3108aade56a8d70babc
satyamgupta0704 <103809254+satyamgupta0704@users.noreply.github.com>
2023-06-30 20:25:17 +0530

    Add files via upload

---
+++ assets/js/route.js
@@ -0,0 +1,43 @@
+
+'use strict';
+
+import { updateWeather, error404 } from "./app.js";
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474"; //London
+
+const currentLocation = function() {
+   window.navigator.geolocation.getCurrentPosition(res => {
+    const { latitude, longitude } = res.coords; 
+
+    updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+   }, err => {
+    window.location.hash = defaultLocation;
+   });
+}
+
+const searchedLocation = query => updateWeather( ...query.split("&"));
+
+const routes = new Map ([
+    ["/current-location", currentLocation],
+    ["/weather", searchedLocation ]
+])
+
+// QUERY IS THE USER_SEARCH ( STRING )
+
+const checkHash = function () { 
+
+   const requestURL = window.location.hash.slice(1);
+    
+   const[route, query] = requestURL.includes ? requestURL.split("?") : [requestURL];
+
+   routes.get(route) ? routes.get(route)(query) : error404() ;
+}
+
+window.addEventListener("hashchange" , checkHash );
+
+window.addEventListener("load" , function () {
+    if(!window.location.hash) {
+        this.window.location.hash = "#/current-location";
+    } else {
+        checkHash() ;
+    }
+})
